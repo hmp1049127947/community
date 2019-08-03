32b73ebc4a8efc6bea9aef9fe5ef98d8cc7dab4d
hmp <hmp1049127947@163.com>
2019-08-03 15:20:22 +0800

    Add bootstrap

--- src/main/java/life/majiang/community/controller/HelloController.java
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -11,9 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String hello(String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello";
+    @RequestMapping("/")
+    public String index() {
+        return "index";
     }
 }
